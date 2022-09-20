74274f8b59854e281853711594c547e2bf6e0d78
ysoru <youngrae.seol@encored.co.jp>
2022-09-20 10:44:05 +0900

    fix

--- src/ejhelper2/aws/dynamodb.py
+++ src/ejhelper2/aws/dynamodb.py
@@ -19,7 +19,7 @@ class DynamoDBTableV2:
         # PROJECT_PREFIXは毎回取得
         PJ_PREFIX: str = getEnv(PROJECT_PREFIX)  # （Global定義禁止）
 
-        if self.table is None:
+        if hasattr(self, 'table') is False:
             self.table: dict[str, Any] = dict()
 
         env_tablename: str = getEnv(

7f29a098de97fb2460efc092a52067cce6bc4f87
sungon.lee <sungon.lee@encored.co.jp>
2023-07-31 17:23:20 +0900

    script

---
+++ script/MakeAnalyzeLambdaLayer.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+
+usages() {
+  echo "Usage.."
+  echo " $1 [arm64|x86_64]"
+  echo " $2 [python3_6|python3_7|python3_8|python3_9]"
+  echo " $3 [dev|test|stg|prod]"
+  echo " $4 [cloud|""]"
+
+  echo " e.g.)"
+  echo "  ./MakeanalyzeLambdaLayer.sh arm64 3.9 dev"
+
+  exit 1
+}
+
+if [ "$1" = "" ]; then
+  usages
+fi
+
+# The layer name can contain only letters, numbers, hyphens, and underscores.
+LAYER_NAME="layer_$1_$2_common"
+echo "LAYER_NAME:" $LAYER_NAME
+
+AWS_PROFILE=""
+if [ "$3" = "dev" ]; then
+  AWS_PROFILE="dev-derms-profile"
+elif [ "$3" = "test" ]; then
+  AWS_PROFILE="test-derms-profile"
+elif [ "$3" = "stg" ]; then
+  AWS_PROFILE="stg-derms-profile"
+elif [ "$3" = "prod" ]; then
+  AWS_PROFILE="prod-derms-profile"
+else
+  usages
+fi
+echo "AWS_PROFILE:" $AWS_PROFILE
+
+zip_dir=$PWD/lambdaLayer/analyzeLambdaLayer/python
+zip_filelist=""
+cd lambdaLayer/analyzeLambdaLayer
+for file in "python/*"; do
+    echo $file
+    zip_filelist+="$file "
+    echo $zip_filelist
+done
+zip -r "layer.zip" $zip_filelist
+
+ZIP_FILE_DIR="$PWD/layer.zip"
+echo "ZIP_FILE_DIR:" $ZIP_FILE_DIR
+if [ "$3" = "cloud" ]; then 
+    aws lambda publish-layer-version --layer-name $LAYER_NAME --zip-file "fileb://"$ZIP_FILE_DIR
+else
+    aws lambda publish-layer-version --layer-name $LAYER_NAME --zip-file "fileb://"$ZIP_FILE_DIR --profile $AWS_PROFILE
+fi
--- script/MakeCommonLambdaLayer.sh
+++ script/MakeCommonLambdaLayer.sh
@@ -1,8 +1,43 @@
 #!/bin/bash
 
-pwd=$PWD
-zip_dir=$pwd/lambdaLayer/commonLambdaLayer/python
-zip_vfilelist=""
+
+usages() {
+  echo "Usage.."
+  echo " $1 [arm64|x86_64]"
+  echo " $2 [python3_6|python3_7|python3_8|python3_9]"
+  echo " $3 [dev|test|stg|prod]"
+  echo " $4 [cloud|""]"
+
+  echo " e.g.)"
+  echo "  ./MakeCommonLambdaLayer.sh arm64 3.9 dev"
+
+  exit 1
+}
+
+if [ "$1" = "" ]; then
+  usages
+fi
+
+# The layer name can contain only letters, numbers, hyphens, and underscores.
+LAYER_NAME="layer_$1_$2_common"
+echo "LAYER_NAME:" $LAYER_NAME
+
+AWS_PROFILE=""
+if [ "$3" = "dev" ]; then
+  AWS_PROFILE="dev-derms-profile"
+elif [ "$3" = "test" ]; then
+  AWS_PROFILE="test-derms-profile"
+elif [ "$3" = "stg" ]; then
+  AWS_PROFILE="stg-derms-profile"
+elif [ "$3" = "prod" ]; then
+  AWS_PROFILE="prod-derms-profile"
+else
+  usages
+fi
+echo "AWS_PROFILE:" $AWS_PROFILE
+
+zip_dir=$PWD/lambdaLayer/commonLambdaLayer/python
+zip_filelist=""
 cd lambdaLayer/commonLambdaLayer
 for file in "python/*"; do
     echo $file
@@ -11,5 +46,10 @@ for file in "python/*"; do
 done
 zip -r "layer.zip" $zip_filelist
 
-
-aws lambda publish-layer-version --layer-name "fefew" --zip-file "fileb://$HOME/WorkSpace/forDevelopment/encored-helper-py/lambdaLayer/commonLambdaLayer/layer.zip" --profile "dev-derms-profile" 
+ZIP_FILE_DIR="$PWD/layer.zip"
+echo "ZIP_FILE_DIR:" $ZIP_FILE_DIR
+if [ "$3" = "cloud" ]; then 
+    aws lambda publish-layer-version --layer-name $LAYER_NAME --zip-file "fileb://"$ZIP_FILE_DIR
+else
+    aws lambda publish-layer-version --layer-name $LAYER_NAME --zip-file "fileb://"$ZIP_FILE_DIR --profile $AWS_PROFILE
+fi
